757db25d9bc661a61df533acb3f882072ea9b7c2
Thorne Brandt <tbrandt@searshc.com>
2014-02-13 14:25:10 -0600

    finished user staging

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
   # get "static_pages/home"
   # get "static_pages/help"
   # get "static_pages/about"
   # get "static_pages/contact"
 
+  resources :users
   root 'static_pages#home'
   match '/signup', to: 'users#new', via: 'get'
   match '/help', to: 'static_pages#help', via: 'get'
