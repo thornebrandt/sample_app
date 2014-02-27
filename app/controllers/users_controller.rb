fd9ddd1b7b5d0dd659679074ff57073ef68c63f9
Thorne Brandt <tbrandt@searshc.com>
2014-02-27 12:09:30 -0600

    got tests to pass

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
 
   def show
   	@user = User.find(params[:id])
@@ -10,6 +12,8 @@ class UsersController < ApplicationController
 
   def create
   	@user = User.new(user_params)
+    puts @user.inspect
+    puts cookies.inspect
   	if @user.save
       sign_in @user
   		flash[:success] = "Welcome to Hell, Bitch!"
@@ -19,8 +23,34 @@ class UsersController < ApplicationController
   	end
   end
 
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   private
   	def user_params
   		params.require(:user).permit(:name, :email, :password, :password_confirmation)
   	end
+
+    # Before filters
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -14,11 +14,32 @@ module SessionsHelper
 	end
 
 	def current_user
+		remember_token = User.encrypt(cookies[:remember_token])
 		@current_user ||= User.find_by_remember_token(cookies[:remember_token])
 	end
 
+	def current_user?(user)
+		user == current_user
+	end
+
 	def sign_out
-		self.current_user = nil
+		current_user.update_attribute(:remember_token, 
+										User.encrypt(User.new_remember_token))
 		cookies.delete(:remember_token)
+		self.current_user = nil
+	end
+
+	def destroy
+		sign_out
+		redirect_to root_url
+	end
+
+	def redirect_back_or(default)
+		redirect_to(session[:return_to] || default)
+		session.delete(:return_to)
+	end
+
+	def store_location
+		session[:return_to] = request.url if request.get?
 	end
 end
